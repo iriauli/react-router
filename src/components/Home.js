2e46871269af4955c9900a295b536448e5825ef1
iriauli <irakliiriauli@gmail.com>
2022-01-19 03:56:02 +0400

    react router v6

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
+export const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div>Home Page</div>
+      <button onClick={() => navigate("order-summary")}>Place order</button>
+    </>
+  );
+};
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+    </nav>
+  );
+};
